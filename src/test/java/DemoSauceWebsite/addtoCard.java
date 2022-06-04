135dab46a0802661471079e24e52e4a45f322828
Walidmazit <wmazit@yahoo.fr>
2022-06-04 17:24:44 -0400

    push

---
+++ src/test/java/DemoSauceWebsite/addtoCard.java
@@ -0,0 +1,18 @@
+package DemoSauceWebsite;
+
+import Base.CommonAPI;
+import base.CommonAPI;
+import org.testng.annotations.Test;
+
+public class addtoCard extends CommonAPI {
+
+
+    @Test
+    public void dropdown() {
+        type("#user-name", "standard_user");
+        type("#password", "secret_sauce");
+        click("#login-button");
+        click("//*[@id=\"shopping_cart_container\"]/a");
+
+    }
+}
